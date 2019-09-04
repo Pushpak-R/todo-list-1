e23e7f71c8489d9b9d597e24af07f60b9268192d
Pushpak Rakesh <pushpak.rakesh@paytm.com>
2019-09-04 14:25:34 +0530

    Updates

---
+++ precache-manifest.637c1ac94a7b5d08db8699f45431d798.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3725ced567f0b9eb6699fd9c4634afa7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "520117020e2a2289d05e",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "137e149705fc93129642",
+    "url": "/static/js/2.597e021f.chunk.js"
+  },
+  {
+    "revision": "520117020e2a2289d05e",
+    "url": "/static/js/main.f95317bd.chunk.js"
+  },
+  {
+    "revision": "1459d5785bc55432c401",
+    "url": "/static/js/runtime~main.2dc77212.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
